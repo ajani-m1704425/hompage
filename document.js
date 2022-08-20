ee81870aead7c16327a87e78b97e4443bfc1c08a
Ajani Malik <malikolalekana4@gmail.com>
2022-08-20 21:46:23 +0100

    almostdone

--- document.js
+++ document.js
@@ -1,23 +1,15 @@
 let drop1 = document.querySelector(".profile_displayname_abbr");
 let drop2 = document.querySelector(".profile_icon");
-let menu = document.querySelector(".mobile_navicon");
-const mainNav = document.querySelector(".inside_main_nav");
+let menu = document.querySelector(".nav_icon");
+let close = document.querySelector(".close");
+const mainNav = document.querySelector(".wat");
 let circle_cancel = document.querySelector(".circle_closed");
 const dropdown = document.querySelector(".profile_dropdown_container");
 menu.addEventListener('click',showmenu);
+close.addEventListener('click',showmenu);
 drop1.addEventListener("click", showdropdown);
 drop2.addEventListener("click", showdropdown);
 circle_cancel.addEventListener("click", showdropdown);
-
-
-function showdropdown(){
-     if (dropdown.style.display == "none"){
-        dropdown.style.display = "block"
-    }
-    else{
-        dropdown.style.display = "none"
-    }
-}
 
 function showmenu(){
     if (mainNav.style.display == "none"){
@@ -28,5 +20,16 @@ function showmenu(){
     }
 
 }
+
+function showdropdown(){
+     if (dropdown.style.display == "none"){
+        dropdown.style.display = "block"
+    }
+    else{
+        dropdown.style.display = "none"
+    }
+}
+
+
 
 
