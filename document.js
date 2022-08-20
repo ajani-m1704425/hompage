50ca053ab3004f33061d3663c1bf97b7a1d3a2b9
Ajani Malik <malikolalekana4@gmail.com>
2022-08-20 20:36:03 +0100

    menu

--- document.js
+++ document.js
@@ -1,7 +1,10 @@
 let drop1 = document.querySelector(".profile_displayname_abbr");
 let drop2 = document.querySelector(".profile_icon");
+let menu = document.querySelector(".mobile_navicon");
+const mainNav = document.querySelector(".inside_main_nav");
 let circle_cancel = document.querySelector(".circle_closed");
 const dropdown = document.querySelector(".profile_dropdown_container");
+menu.addEventListener('click',showmenu);
 drop1.addEventListener("click", showdropdown);
 drop2.addEventListener("click", showdropdown);
 circle_cancel.addEventListener("click", showdropdown);
@@ -15,3 +18,15 @@ function showdropdown(){
         dropdown.style.display = "none"
     }
 }
+
+function showmenu(){
+    if (mainNav.style.display == "none"){
+        mainNav.style.display = "block"
+    }
+    else{
+        mainNav.style.display = "none"
+    }
+
+}
+
+
